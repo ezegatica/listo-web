643266d0e9acada5ad084fa3a742baa0e8c52e0c
Gati <ezequielgatica@gmail.com>
2020-08-01 03:06:05 -0300

    Update Home.js

--- src/Components/Pages/Home.js
+++ src/Components/Pages/Home.js
@@ -1,9 +1,10 @@
 import React from 'react'
-
+import {Link} from 'react-router-dom'
 const Home = () =>{
     return(
         <div>
             <h1 className="center">Home</h1>
+            <Link to="/proyectos/nuevo"><h4 className="center">Nuevo Proyecto?</h4></Link>
         </div>
     )
 }
