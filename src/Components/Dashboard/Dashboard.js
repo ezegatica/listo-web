7968c6dcb382e5e16b00e8795b8f31d79cef9ecf
Gati <ezequielgatica@gmail.com>
2020-08-01 02:33:11 -0300

    APARECEN PA

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react'
 import Notifications from './Notifications'
 import List from '../Proyects/List'
 import {connect} from 'react-redux'
+import { firestoreConnect } from 'react-redux-firebase'
+import {compose} from 'redux'
 
 class Dashboard extends Component{
     render(){
@@ -23,9 +25,15 @@ class Dashboard extends Component{
 }
 
 const mapStateToProps= (state) =>{
+    console.log(state)
     return {
-        projects: state.project.projects
+        projects: state.firestore.ordered.proyectos
     }
 }
 
-export default connect(mapStateToProps)(Dashboard)
+export default compose(
+    connect(mapStateToProps),
+    firestoreConnect([
+        {collection: 'proyectos'}
+    ])
+)(Dashboard)
