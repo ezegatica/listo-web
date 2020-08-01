e9fd1f3b39e20a44275289247fe32cc1a3b5ae8d
Gati <ezequielgatica@gmail.com>
2020-08-01 03:27:32 -0300

    Update Summary.js

--- src/Components/Proyects/Summary.js
+++ src/Components/Proyects/Summary.js
@@ -5,7 +5,7 @@ const Summary = ({project}) =>{
         <div className="card z-depth-0 proyect-summary">
                 <div className="card-content grey-text text-darken-3">
                     <span className="card-title">{project.title}</span>
-                    <p>Posted by: Admin</p>
+                    <p>Posted by: {project.autorNombre}</p>
                     <p className="grey-text">El 3 de febrero</p>
                     <p>[DEBUG] ID: {project.id}</p>
                 </div>
