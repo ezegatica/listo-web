4b2f632ac8d08a7418df05c85829c0417bd7bd0b
Gati <ezequielgatica@gmail.com>
2020-08-06 15:04:41 -0300

    updated firebase

--- src/Config/fbConfig.js
+++ src/Config/fbConfig.js
@@ -3,14 +3,13 @@ import 'firebase/firestore'
 import 'firebase/auth'
 // Your web app's Firebase configuration
 var config = {
-    apiKey: "AIzaSyBnuAahCVSPbOE-q9OOwgObPIMMSPE-YYY",
-    authDomain: "gati-planner.firebaseapp.com",
-    databaseURL: "https://gati-planner.firebaseio.com",
-    projectId: "gati-planner",
-    storageBucket: "gati-planner.appspot.com",
-    messagingSenderId: "104060325210",
-    // appId: "1:104060325210:web:fe06310e486fa7a21df763",
-    // measurementId: "G-2T5JLBTRMB"
+    apiKey: "AIzaSyAcPMH0qYuAqvTCJaq9LSLZECh0V9cApV0",
+    authDomain: "prueba-proyecto-tic.firebaseapp.com",
+    databaseURL: "https://prueba-proyecto-tic.firebaseio.com",
+    projectId: "prueba-proyecto-tic",
+    storageBucket: "prueba-proyecto-tic.appspot.com",
+    messagingSenderId: "68477335290",
+    appId: "1:68477335290:web:fb7914e85889cc30293b43"
   };
   // Initialize Firebase
   firebase.initializeApp(config);
