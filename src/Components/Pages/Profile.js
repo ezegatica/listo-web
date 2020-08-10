885c662329fded72d10637924363e47306ce1e7b
Gati <ezequielgatica@gmail.com>
2020-08-10 18:31:01 -0300

    boton verificar email, bug: no aparecee el mensaje

--- src/Components/Pages/Profile.js
+++ src/Components/Pages/Profile.js
@@ -2,23 +2,34 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { Redirect } from 'react-router-dom'
 import Lista from '../Productos/Lista'
+import { auth } from '../../Config/fbConfig'
 
 
+const sendMail = (credentials) => {
+    console.log("verificar", credentials)
+    auth.currentUser.sendEmailVerification()
+    .then(() => {
+        console.log("MAIL ENVIADO!")
+    }).catch((err)=>{
+        console.log("ERROR: ", err.message)
+    })
+}
 const Profile = (props) => {
-    // console.log("PROFILE.JS PROPS:", props)
-    const admin = props.profile.isAdmin ? <div>Hola admin!</div> : null
+    const admin = props.profile.isAdmin ? <div>Admin Mode Owo</div> : null
     if (!props.auth.uid && props.auth.isLoaded) {
         return <Redirect to="/login" />
     }
     else {
         if (props.profile.isLoaded) {
-                // console.log("ES ADMIN?", props.profile.isAdmin)
+            console.log(props.auth.emailVerified)
             return (
                 <div className="container nav-center">
                     <div className="carta">
                         <h1 title={props.profile.nombre}>{props.profile.nombre}</h1>
                         <p className="titulo">{props.auth.email}</p>
+                        {!props.auth.emailVerified ? <button className="waves-effect waves-green btn grey lighten-4 black-text btn-small" onClick={() => sendMail(props.auth.email)}> Verificar mail</button> : null}
                         <div>{admin}</div>
+                        <br/>
                     </div>
                     <div>
                         <h4 className="center">Mis productos:</h4>
