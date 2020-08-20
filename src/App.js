f34dc4f2005b541b1be226ac63832404954de15c
Gati <ezequielgatica@gmail.com>
2020-08-20 09:41:14 -0300

    mantenimiento-2

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import {BrowserRouter, Switch, Route} from 'react-router-dom'
-import Navbar from './Components/Layout/Navbar'
-import MobileSpacing from './Components/Layout/MobileSpacing'
+// import Navbar from './Components/Layout/Navbar'
+// import MobileSpacing from './Components/Layout/MobileSpacing'
 import Home from './Components/Pages/Home'
 
-import Dashboard from './Components/Dashboard/Dashboard'
-import Profile from './Components/Pages/Profile'
-import RestaurantesLista from './Components/Pages/RestaurantesLista'
-import RestauranteDetalles from './Components/Pages/RestauranteDetalles'
-import ProductoDetalles from './Components/Productos/Detalles'
-import Login from './Components/Auth/SignIn'
-import Register from './Components/Auth/SignUp'
-import Recovery from './Components/Auth/Recovery'
-import Agregar from './Components/Productos/Agregar'
+// import Dashboard from './Components/Dashboard/Dashboard'
+// import Profile from './Components/Pages/Profile'
+// import RestaurantesLista from './Components/Pages/RestaurantesLista'
+// import RestauranteDetalles from './Components/Pages/RestauranteDetalles'
+// import ProductoDetalles from './Components/Productos/Detalles'
+// import Login from './Components/Auth/SignIn'
+// import Register from './Components/Auth/SignUp'
+// import Recovery from './Components/Auth/Recovery'
+// import Agregar from './Components/Productos/Agregar'
 // import Borrar from './Components/Productos/Borrar'
 // import Create from './Components/Proyects/Create'
 // import Editar from './Components/Proyects/Edit.js'
@@ -31,7 +31,7 @@ function App() {
       </div>
       <Switch>
         <Route exact path="/" component={Home} />
-        <Route path="/dashboard" component={Dashboard} />
+        {/* <Route path="/dashboard" component={Dashboard} />
         <Route path="/profile" component={Profile} />
         <Route path="/login" component={Login} />
         <Route path="/register" component={Register} />
@@ -39,7 +39,7 @@ function App() {
         <Route exact path="/productos/nuevo" component={Agregar} />
         <Route exact path="/restaurantes" component={RestaurantesLista} />
         <Route exact path="/restaurantes/:id" component={RestauranteDetalles} />
-        <Route exact path="/restaurantes/:id/:productoid" component={ProductoDetalles} />
+        <Route exact path="/restaurantes/:id/:productoid" component={ProductoDetalles} /> */}
 
         {/* <Route exact path="/restaurantes/:id/:productoid/editar" component={ProductoEditar} /> */}
         {/* <Route exact path="/restaurantes/:id/:productoid/borrar" component={Borrar} /> */}
