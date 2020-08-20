2c5b50af645588f37734d16efe3ffea5d58a6283
Gati <ezequielgatica@gmail.com>
2020-08-20 09:40:41 -0300

    mantenimiento

--- src/App.js
+++ src/App.js
@@ -2,28 +2,32 @@ import React from 'react';
 import {BrowserRouter, Switch, Route} from 'react-router-dom'
 import Navbar from './Components/Layout/Navbar'
 import MobileSpacing from './Components/Layout/MobileSpacing'
+import Home from './Components/Pages/Home'
+
 import Dashboard from './Components/Dashboard/Dashboard'
 import Profile from './Components/Pages/Profile'
-// import Proyectos from './Components/Dashboard/Proyectos'
-import Home from './Components/Pages/Home'
 import RestaurantesLista from './Components/Pages/RestaurantesLista'
-// import Details from './Components/Proyects/Details'
 import RestauranteDetalles from './Components/Pages/RestauranteDetalles'
 import ProductoDetalles from './Components/Productos/Detalles'
-// import ProductoEditar from './Components/Productos/Editar'
 import Login from './Components/Auth/SignIn'
 import Register from './Components/Auth/SignUp'
 import Recovery from './Components/Auth/Recovery'
-// import Create from './Components/Proyects/Create'
-// import Editar from './Components/Proyects/Edit.js'
 import Agregar from './Components/Productos/Agregar'
 // import Borrar from './Components/Productos/Borrar'
+// import Create from './Components/Proyects/Create'
+// import Editar from './Components/Proyects/Edit.js'
+// import ProductoEditar from './Components/Productos/Editar'
+// import Details from './Components/Proyects/Details'
+// import Proyectos from './Components/Dashboard/Proyectos'
+
+
+
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        <Navbar />
-        <MobileSpacing />
+        {/* <Navbar />
+        <MobileSpacing /> */}
       </div>
       <Switch>
         <Route exact path="/" component={Home} />
@@ -31,17 +35,18 @@ function App() {
         <Route path="/profile" component={Profile} />
         <Route path="/login" component={Login} />
         <Route path="/register" component={Register} />
-        {/* <Route exact path='/proyectos' component={Proyectos}/>
-        <Route exact path='/proyectos/nuevo' component={Create}/>
-        <Route path="/proyectos/:id" component={Details} />
-        <Route path="/editar/:id" component={Editar} /> */}
         <Route path="/recovery" component={Recovery} />
         <Route exact path="/productos/nuevo" component={Agregar} />
         <Route exact path="/restaurantes" component={RestaurantesLista} />
         <Route exact path="/restaurantes/:id" component={RestauranteDetalles} />
         <Route exact path="/restaurantes/:id/:productoid" component={ProductoDetalles} />
+
         {/* <Route exact path="/restaurantes/:id/:productoid/editar" component={ProductoEditar} /> */}
         {/* <Route exact path="/restaurantes/:id/:productoid/borrar" component={Borrar} /> */}
+        {/* <Route exact path='/proyectos' component={Proyectos}/>
+        <Route exact path='/proyectos/nuevo' component={Create}/>
+        <Route path="/proyectos/:id" component={Details} />
+        <Route path="/editar/:id" component={Editar} /> */}
       </Switch>
     </BrowserRouter>
     
--- src/Components/Pages/Home.js
+++ src/Components/Pages/Home.js
@@ -1,23 +1,21 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
+// import {Link} from 'react-router-dom'
 const Home = () =>{
     return(
         <div>
             <div className="red">
-            <h1 className="container center">AVISO!</h1>
+            <h1 className="container center">Sitio web en mantenimiento</h1>
             <h4 className="center">La base de datos se reinciara el 20-8 para tareas de mantenimiento y actualizacion, se perderan todos los datos y cuentas existentes</h4>
             <br />
             </div>
-            <hr />
+            {/* <hr />
             <Link to="/productos/nuevo"><h3 className="center">Crear Producto</h3></Link>
             <Link to="/profile"><h3 className="center">Mi perfil :D</h3></Link>
             <Link to="/restaurantes"><h3 className="center">Restaurantes</h3></Link>
-            {/* <Link to="/proyectos/nuevo"><h4 className="center">Nuevo Proyecto?</h4></Link>
-            <Link to="/proyectos"><h4 className="center">Lista de Proyectos</h4></Link> */}
 
             <h3>Email: usu@r.io</h3>
             <h3>Contraseña: usuario</h3>
-            <h5><a href="https://react.gati.ga">Otros proyectos</a></h5>
+            <h5><a href="https://react.gati.ga">Otros proyectos</a></h5> */}
         </div>
     )
 }
