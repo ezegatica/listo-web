98ce4f4b2999dac4014ff9a7fe24ff91747a9f82
Gati <ezequielgatica@gmail.com>
2020-09-29 10:53:14 -0300

    boton borrar todos los favoritos

--- src/Components/Pages/P_User.js
+++ src/Components/Pages/P_User.js
@@ -1,33 +1,32 @@
 import React, { Component } from 'react'
 import ShowRestaurante from '../Restaurante/ShowRestaurante'
-import { db, auth } from '../../Config/fbConfig'
+import { db, auth, fb } from '../../Config/fbConfig'
 // import { connect } from 'react-redux'
 import swal from 'sweetalert';
 
 export class PerfilUsuario extends Component {
     state = {
-        restaurante: null,
-        update: false,
-        updated: false
+        restaurantes: null,
     }
-    
-    
+
+
     leerDB = () => {
         const Restaurantes = []
         const favs = this.props.profile.favoritos
+        this.setState({ restaurantes: null })
         console.log("favoritos", favs);
-        if (favs){
+        if (favs) {
             favs.forEach(id => {
                 db.collection('restaurantes').doc(id).get()
-                .then(snapshot => {
-                    const info = snapshot.data()
-                    const id = snapshot.id;
-                    Restaurantes.push({ info, id })
-                    this.setState({ restaurantes: Restaurantes })
-                }).catch(error => console.log(error))
+                    .then(snapshot => {
+                        const info = snapshot.data()
+                        const id = snapshot.id;
+                        Restaurantes.push({ info, id })
+                        this.setState({ restaurantes: Restaurantes })
+                    }).catch(error => console.log(error))
             })
         }
-        
+
     }
     componentDidMount() {
         this.leerDB()
@@ -36,32 +35,35 @@ export class PerfilUsuario extends Component {
         const uid = auth.currentUser.uid
         console.log(uid);
         swal({
-            title: "Are you sure?",
-            text: "Once deleted, you will not be able to recover this imaginary file!",
+            title: "Cuidado!",
+            text: "Estas seguro que quieres eliminar todos tus favoritos? Una vez hecho, no se puede volver atras...",
             icon: "warning",
-            buttons: true,
+            buttons: ["Cancelar", true],
             dangerMode: true,
-          })
-          .then((willDelete) => {
-            if (willDelete) {
-              swal("Poof! Your imaginary file has been deleted!", {
-                icon: "success",
-              });
-            } else {
-            //   swal("Your imaginary file is safe!");
-            }
-          });
+        })
+            .then((willDelete) => {
+                if (willDelete) {
+                    db.collection('usuarios').doc(uid).update({ "favoritos": fb.firestore.FieldValue.delete() }).then(() => {
+                        this.leerDB()
+                    }).catch((err) => { console.log(err); })
+                    swal("Favoritos borrado!", {
+                        icon: "success",
+                    });
+                } else {
+
+                }
+            });
     }
     render() {
-        
+
         const perfil = this.props.profile
         return (
             <>
-                <h4 className="center">Mis restaurantes favoritos:</h4> 
-                <div className="hover" style={{userSelect: 'none'}} onClick={this.clearFavs}><p className="red-text"><i className="material-icons">delete</i>Borrar todos los favoritos</p></div>
+                <h4 className="center">Mis restaurantes favoritos:</h4>
+                <div className="hover" style={{ userSelect: 'none' }} onClick={this.clearFavs}><p className="red-text"><i className="material-icons">delete</i>Borrar todos los favoritos</p></div>
                 {this.state.restaurantes && this.state.restaurantes.map(restaurant => {
                     return (
-                        <ShowRestaurante restaurant={restaurant} key={restaurant.id} perfil={perfil}/>
+                        <ShowRestaurante restaurant={restaurant} key={restaurant.id} perfil={perfil} />
                     )
                 })}
             </>
