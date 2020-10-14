fa362764628170219be8fc4d00296dd856942f61
Eze Gatica <ezequielgatica@gmail.com>
2020-10-14 18:24:41 -0300

    sigue habiendo un error y no puede ser tan rancio, lo arreglo dsp

--- src/Components/Carrito/CartWrapper.js
+++ src/Components/Carrito/CartWrapper.js
@@ -5,7 +5,7 @@ import Cart from './Cart2'
 import { auth } from '../../Config/fbConfig'
 import '../../css/cart.css'
 import { db } from '../../Config/fbConfig'
-
+let updated = false
 let done = false
 export class CartWrapper extends Component {
     state = {
@@ -15,11 +15,12 @@ export class CartWrapper extends Component {
         B: false,
     }
     leerDB() {
-        // console.log("leyendo db");
+        console.log("pedido leer db");
         if (!done && !this.props.prevent) {
-            // console.log("EJECUTANDO FUNCION!");
+            console.log("EJECUTANDO FUNCION!");
             done = true
             let productos = []
+            this.setState({productos: null})
             if (this.props.profile.cart) {
                 this.props.profile.cart.map((item) => {
                     db.collection('restaurantes').doc(item.restaurante).collection('productos').doc(item.producto).get()
@@ -32,13 +33,25 @@ export class CartWrapper extends Component {
                 })
                 this.setState({ productos, B: true })
             }
+        }else{
+            console.log("DENIED!");
         }
     }
     leerProfile = () => {
         this.setState({cart: this.props.profile.cart})
     }    
     componentDidUpdate=()=>{
-        
+        if (this.props.prevent){
+            updated = true
+        }
+        if (!this.props.prevent && updated === true){
+            updated = false;
+            this.leerDB()
+            console.log("STATE AL LEER DE VUELTA: ", this.state);
+            console.log('%c LEER LA BASE DE DATOS DE VUELTA ', 'background: #222; color: #bada55');
+                this.setState({
+                    A: false})
+        }
     }
     componentWillUnmount = () => {
         this.setState({
@@ -49,10 +62,9 @@ export class CartWrapper extends Component {
         done = false
     }
     render =()=> {
-        // console.log("PREVENIR:" ,this.props.prevent);
-        // console.log("STATE: ", this.state);
+        console.log("CART:" ,this.props.profile.cart);
+        console.log("PRODUCTOS: ", this.state.productos);
         const { A, B } = this.state
-
         if (this.props.profile.isLoaded && !this.props.profile.isResto && !this.props.profile.isEmpty) {
             // console.log("CART: ", this.props.profile.cart);
             if (this.state.productos && this.state.productos[0]) {
@@ -67,11 +79,22 @@ export class CartWrapper extends Component {
                     }
                     this.leerDB()
                 }
+                if (this.props.prevent){
+                    return(
+                        <div className="caja">
+                        <div className="centrado">
+                            <div className="loadingio-spinner-bars-jl0izsh3cc"><div className="ldio-at0j3uszb4c">
+                                <div></div><div></div><div></div><div></div>
+                            </div></div>
+                        </div>
+                    </div>  
+                    )
+                }else{
                 return (
                     <div className="container">
                         <Cart profile={this.props.profile} auth={auth} productos={this.state.productos} />
                     </div>
-                )
+                )}
             } else {
                 this.leerDB()
 
